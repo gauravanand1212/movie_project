340b38be5e3c09cd8ce555518bac2743c546d60a
gauravanand1212 <gaurav.anand@gmail.com>
2015-10-21 11:56:27 -0700

    Update entertainment_center.py

--- entertainment_center.py
+++ entertainment_center.py
@@ -5,19 +5,22 @@ import fresh_tomatoes
 
 # Create 3 movie objects
 shawshank = media.movie("Shawshank Redemption",
-                        "http://t0.gstatic.com/images?q=tbn:ANd9GcSkmMH-bEDUS2TmK8amBqgIMgrfzN1_mImChPuMrunA1XjNTSKm",
+                        "http://t0.gstatic.com/images?q=tbn:ANd9GcSkmMH-\
+                        bEDUS2TmK8amBqgIMgrfzN1_mImChPuMrunA1XjNTSKm",
                         "https://www.youtube.com/watch?v=6hB3S9bIaco")
 
 jurassic = media.movie("Jurassic Park",
-                       "http://t1.gstatic.com/images?q=tbn:ANd9GcT9t_mvZu0k-wS_QqvQmPKc6FMkwJoj0HgUjwLJK7uGG_NtZBxV",
+                       "http://t1.gstatic.com/images?q=tbn:ANd9GcT9t_mvZu0k-\
+                       wS_QqvQmPKc6FMkwJoj0HgUjwLJK7uGG_NtZBxV",
                        "www.youtube.com/watch?v=lc0UehYemQA")
 
 insideman = media.movie("Inside Man",
-                        "http://www.gstatic.com/tv/thumb/movieposters/159788/p159788_p_v7_aa.jpg",
+                        "http://www.gstatic.com/tv/thumb/movieposters/159788/\
+                        p159788_p_v7_aa.jpg",
                         "www.youtube.com/watch?v=3WRxsmqercg")
 
-#create a list of movies to display
+# create a list of movies to display
 movie_list = [shawshank, jurassic, insideman]
 
-#Open movie page with created list
+# Open movie page with created list
 fresh_tomatoes.open_movies_page(movie_list)
